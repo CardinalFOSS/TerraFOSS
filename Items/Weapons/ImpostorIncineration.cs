95a6c4c18e8567173b9799a2334c360f04847951
MaybeJamm <mjammer24@gmail.com>
2022-02-20 01:28:46 -0500

    +flamethrower & projectile
    new flamethrower using bars from before and a projectile for that flamethrower

---
+++ Items/Weapons/ImpostorIncineration.cs
@@ -0,0 +1,42 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TerraFOSS.Items.Misc;
+
+namespace TerraFOSS.Items.Weapons
+{
+    public class ImpostorIncineration : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.damage = 25;
+            item.ranged = true;
+            item.width = 45;
+            item.height = 17;
+            item.useTime = 10;
+            item.useAnimation = 30;
+            item.useStyle = 5;
+            item.noMelee = true;
+            item.knockBack = 2;
+            item.value = 10000;
+            item.rare = 2;
+            item.autoReuse = true;
+            item.shoot = mod.ProjectileType("AmUsFirey");
+            item.useAmmo = AmmoID.Gel;
+            item.shootSpeed = 15f;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ModContent.ItemType<AmongiteBar>(), 25);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("For setting Impostors on fire!");
+        }
+    }
+}
---
+++ Projectiles/AmUsFirey.cs
@@ -0,0 +1,30 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TerraFOSS.Projectiles
+{
+    public class AmUsFirey : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("hot cheeto infused");
+        }
+        public override void SetDefaults()
+        {
+            projectile.arrow = true;
+            projectile.width = 9;
+            projectile.height = 9;
+            projectile.aiStyle = 23;
+            projectile.friendly = true;
+            projectile.ranged = true;
+            aiType = ProjectileID.Flames;
+            projectile.penetrate = 6;
+            projectile.timeLeft = 160;
+        }
+        public override void PostAI()
+        {
+            projectile.tileCollide = true;
+        }
+    }
+}
